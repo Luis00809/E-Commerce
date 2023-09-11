658a56dcc120ce320760b51c76b40b4c0855cdaf
Luis00809 <davidcarvajal008@gmail.com>
2023-09-11 12:16:53 -0700

    set up table constraints

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Tag, {
+  foreignKey: 'price'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.hasMany(Product, {
+  foreignKey: 'price',
+})
 
 module.exports = {
   Product,
